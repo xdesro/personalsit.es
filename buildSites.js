6e5a23aaf9c2ddeaaeac2108ec1d0b4fdf57cb1d
Henry Desroches <yo@henry.codes>
2020-12-18 11:26:02 -0700

    🏗  - Fix build for Netlify, remove nonexistent site.

--- buildSites.js
+++ buildSites.js
@@ -37,17 +37,23 @@ const getScreenshot = async (site) => {
   //  TODO — Statically render URLs in template files
   const doesntExist = await new Promise((resolve, reject) => {
     http.get(cloudinary.url(site.title), (response) => {
+      if (response.statusCode !== 404) {
+        console.log(`${site.title} exists on cloudinary.`);
+      } else {
+        console.log(`${site.title} DOES NOT exist on cloudinary.`);
+      }
+
       resolve(response.statusCode === 404);
     });
   });
   if (doesntExist) {
     try {
-      const screenshot = await captureWebsite.buffer(site.url, { timeout: 15 });
+      const screenshot = await captureWebsite.buffer(site.url, { timeout: 5 });
       cloudinary.uploader
         .upload_stream({ resource_type: 'image', public_id: site.title })
         .end(screenshot);
     } catch (error) {
-      console.log(site.title, error);
+      console.error(site.title, error);
     }
   }
 };
@@ -57,5 +63,10 @@ const getScreenshot = async (site) => {
     for (const site of sites) {
       await getScreenshot(site);
     }
+    console.log(`
+    =====
+    Screenshots collection complete.
+    =====`);
   })();
+  return;
 })();
--- sites/graphicdesigner.work.md
+++ sites/graphicdesigner.work.md
@@ -1,6 +1,6 @@
 ---
 title: 'graphicdesigner.work'
-url: 'https://graphicdesigner.work'
+url: 'https://graphicdesigner.work/'
 tags: ['graphic designer', 'web designer', 'typographer', 'creative director']
 nsfw: false
 rss: false
